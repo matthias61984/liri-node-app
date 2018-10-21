10ab41e9dc837203ceebbe3d4033e932e8d1a6aa
Corey Walsh <matthias61984@hotmail.com>
2018-10-20 20:20:52 -0700

    incomplete

--- keys.js
+++ keys.js
@@ -4,3 +4,7 @@ module.exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+module.exports.omdb = {
+    apiKey: process.env.OMDB_API_KEY
+};
--- liri.js
+++ liri.js
@@ -1,10 +1,15 @@
 // Required magic beans
   require("dotenv").config();
+  var request = require('request');
 
 // Required spotify magic
   var keys = require("./keys.js")
   var Spotify = require('node-spotify-api');
   var spotify = new Spotify(keys.spotify);
+// Required omdb magic
+  var omdbApi = require('omdb');
+//  var omdb = new omdbApi(keys.omdb);
+  
 
 // Store command input to decide which function to call
   var command = process.argv[2];
@@ -33,9 +38,25 @@
     
   }
 
+// OMDb command!
+  var omdbIt = function(input) {
+    omdb.get(params, function(err, data) {
+       // If stuff gets screwy...
+       if (err) {
+        // ...tell us what's screwy
+          return console.log('Error occurred: ' + err);
+        }
+      // Console log all the cool stuff about the movie
+        console.log("Name of movie: " + data.title);
+    });
+  };
+  
+
 // Depending on the command provided in argv, call appropriate function and pass it input as an argument
   if (command == "spotify-this-song") {
     spotifyIt(input);
   } else if (command == "concert-this") {
     concertIt(input);
+  } else if (command == "movie-this"); {
+    omdbIt(input);
   }
