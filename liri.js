a1981ea6473bde9ade8ab4e0f37eb01af6a57b36
Corey Walsh <matthias61984@hotmail.com>
2018-10-22 20:53:16 -0700

    only need last function

--- liri.js
+++ liri.js
@@ -1,15 +1,16 @@
 // Required magic beans
   require("dotenv").config();
   var request = require('request');
+  var moment = require('moment');
 
 // Required spotify magic
-  var keys = require("./keys.js")
+  var keys = require("./keys.js");
   var Spotify = require('node-spotify-api');
   var spotify = new Spotify(keys.spotify);
   
 // Store command input to decide which function to call
   var command = process.argv[2];
-// Slice off remaining indices in command line input, join together as input 
+// Slice off remaining indices in command line, join together as input for searches
   var input = process.argv.slice(3).join(" ");
 
 // Spotify command!
@@ -31,21 +32,33 @@
 
 // Concert command!
   var concertIt = function(input) {
-    
+  // Call the bands in town api, inserting the input as the search parameter
+    request("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp", function(error, response, body) {
+    // If we get no error...
+      if (!error && response.statusCode === 200) {
+      // ...for the first ten shows, log the desired information to the terminal
+        for (i=0; i <= 9; i++) {
+          console.log(input + " will be playing at " + JSON.parse(body)[i].venue.name + " in " + JSON.parse(body)[i].venue.city + ", " + JSON.parse(body)[i].venue.region + " on " + moment(JSON.parse(body)[i].datetime).format('ll') + ".");
+        }
+      }
+    });
   }
 
 // OMDb command!
   var omdbIt = function(input) {
+  // Call the omdb api, inserting the input as the search parameter
     request("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+    // If we get no error...
       if (!error && response.statusCode === 200) {
-        console.log("The movie's title is: " + JSON.parse(body).Title);
-        console.log("The movie was released in: " + JSON.parse(body).Year);
-        console.log("The movie's imdb rating is: " + JSON.parse(body).Ratings[0].Value);
-        console.log("The movie's rotten tomatoes rating is: " + JSON.parse(body).Ratings[1].Value);
-        console.log("The movie was produced in: " + JSON.parse(body).Country);
-        console.log("The movie's languages are: " + JSON.parse(body).Language);
-        console.log("The movie plot: " + JSON.parse(body).Plot);
-        console.log("The movie's notable actors: " + JSON.parse(body).Actors);
+      // ...log the desired information to the terminal
+        console.log("Movie title: " + JSON.parse(body).Title);
+        console.log("Release date: " + JSON.parse(body).Year);
+        console.log("Imdb rating: " + JSON.parse(body).Ratings[0].Value);
+        console.log("Rotten tomatoes rating: " + JSON.parse(body).Ratings[1].Value);
+        console.log("Year: " + JSON.parse(body).Country);
+        console.log("Languages: " + JSON.parse(body).Language);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
       }
     });
   }
@@ -65,5 +78,6 @@
   } else if (command == "do-what-it-says") {
     doWhatItSays();
   } else {
-    console.log("Please provide a valid command as argv[2]");
+  // Instruct user to enter appropriate command
+    console.log("Please provide a valid command as argv[2], and your search input separated by spaces afterwards.");
   }
